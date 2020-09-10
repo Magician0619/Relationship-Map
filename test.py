6122253fc8acf745fa7e5902ac80529965664b73
Lenovo-HK <kunkun8824@163.com>
2020-09-10 15:40:57 +0800

    2020-09-10 15:40:36 9月最后版本

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import re
 
-restr = '\n小妹。小米。小麦。小刚。大红。大绿。'
+restr = '小妹。小米。小麦。小刚。大红。大绿。AefgB。A。B'
 # pattern = re.compile('[\n。].*占地[\n。]*[。]')
 # pattern = re.compile('[\n。][^。]*占地[^。]*[。]')
 # pattern = re.compile('[^\n。]*占地[^。]*[。]')
 w = "小刚"
-pattern = re.compile('[^\n。]*'+w+'[^。]*[\n]')
+# pattern = re.compile('[^\n。]*'+w+'[^。]*[\n]')
+# pattern = re.compile('[[^。]*[^。]]*'+ w +'[^。]*[。]')
+# pattern = re.compile("[^[^。]*[。]]" + "*" + "[^[^。]*[^。]]")
+a = '[^。]*[。]' 
+b = '[。]*[。]'
+c = '(?<=。).*?。'
+# pattern = re.compile('[^。]*[^。]*[^。]')
+# pattern = re.compile('[^。]*[。]')
+pattern = '[^。]+。小刚。[^。]+。'
 search = re.findall(pattern,restr)
 print(search)
-print(str(search))
 
 # 结果
 # ，占地15万平方米。
